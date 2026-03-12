d83e07c1c6d88bb4bbdd9c27b623cd0591d0c6a9
Zack <sin12014@byui.edu>
2026-03-12 12:37:30 -0700

    Implement GET and POST routes for recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { getAllRecipes, createRecipe } from '../Controllers/recipes.js';
 
 const router = express.Router();
+// TODO: Add recipes routes
 
-// TODO: Add recipes routes here
+router.get('/', getAllRecipes);
+
+rouer.post('/', createRecipe);
 
 export default router;
