d58b67fb3163c3492aa66975cdf0f8de7950b00a
zackadoodledoo <sin12014@byui.edu>
2026-03-18 18:36:04 -0700

    recipes and generic auth

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { getAllRecipes, getRecipeById, createRecipe, updateRecipe, deleteRecipe } from '../Controllers/recipes.js';
-import auth from '../middleware/auth.js';
+//import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getAllRecipes);
 router.get('/:id', getRecipeById);
-router.post('/', auth, createRecipe);
-router.put('/:id', auth, updateRecipe);
-router.delete('/:id', auth, deleteRecipe);
+router.post('/', createRecipe);
+router.put('/:id', updateRecipe);
+router.delete('/:id', deleteRecipe);
 
 // optional premium route
 // router.post('/ai/generate', auth, premium, generateAIRecipe);
